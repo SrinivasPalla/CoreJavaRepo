4d9611def12f2f33ed11956bc0f735307e9f5b1f
Srinivas Palla <srinivas.palla.sp@gmail.com>
2021-10-29 13:57:23 +0530

    commiting22 from GUI

--- JavaTestPalla/src/com/Hello.java
+++ JavaTestPalla/src/com/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello world1111122222");
+		System.out.println("Hello world1111122222GUI -----");
 	}
 
 }
