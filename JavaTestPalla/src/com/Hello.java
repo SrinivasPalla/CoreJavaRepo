88481d3c8cfcce236838a8876ccb2ae371c065cc
Srinivas Palla <srinivas.palla.sp@gmail.com>
2022-01-21 21:54:54 +0530

    commiting files to dev1 branch

--- JavaTestPalla/src/com/Hello.java
+++ JavaTestPalla/src/com/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello world1111122222GUI -----");
+		System.out.println("Hello world1111122222GUI palla -----");
 	}
 
 }
