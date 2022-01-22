391a286cc931a217af61af25f5c3cbfbdf24721c
Srinivas Palla <srinivas.palla.sp@gmail.com>
2022-01-22 13:08:05 +0530

    modified dev3 files

--- JavaTestPalla/src/com/Hello.java
+++ JavaTestPalla/src/com/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        int dev3;
+		
 		System.out.println("Hello world1111122222GUI palla -----");
 	}
 
--- JavaTestPalla/src/service/TestService.java
+++ JavaTestPalla/src/service/TestService.java
@@ -1,5 +1,7 @@
 package service;
 
-public class TestService {
+public class TestService 
+
+int dev3;
 
 }
